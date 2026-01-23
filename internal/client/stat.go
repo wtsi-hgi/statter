597b29e7880b4677c6af19a602716166db514442
Michael Woolnough <mw31@sanger.ac.uk>
2026-01-23 16:50:12 +0000

    Correct ErrClosed -> EOF

--- internal/client/stat.go
+++ internal/client/stat.go
@@ -78,6 +78,10 @@ func writePath(w io.Writer, path string, head bool) error {
 
 	_, err := w.Write(binary.LittleEndian.AppendUint16(buf[:1], uint16(len(path)))) //nolint:gosec
 	if err != nil {
+		if errors.Is(err, fs.ErrClosed) {
+			return io.EOF
+		}
+
 		return err
 	}
 
@@ -134,6 +138,10 @@ func getStat(name string, r io.Reader) (fs.FileInfo, error) {
 	var buf [statBufSize]byte
 
 	if _, err := io.ReadFull(r, buf[:]); err != nil {
+		if errors.Is(err, fs.ErrClosed) {
+			return nil, io.EOF
+		}
+
 		return nil, err
 	}
 
